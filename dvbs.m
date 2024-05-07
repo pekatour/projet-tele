775f951c131e396cfd2a59fa16a7b4b28b85e8b3
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-05-07 15:47:14 +0200

    feat: avancer

--- dvbs.m
+++ dvbs.m
@@ -3,42 +3,155 @@ close all; clc;
 %% Paramètres
 Fe = 24000; % Fréquence d’échantillonnage
 Rb = 3000; % Débit binaire
-EbN0dB = 10; % Niveau de Eb/N0 souhaitée en dB
+EbN0dB = 20; % Niveau de Eb/N0 souhaitée en dB
 M = 4; % Ordre de la modulation
 fp = 2000; % Fréquence porteuse
 Te = 1 / Fe; % Période d’échantillonnage
 Rs = Rb / log2(M); % Débit symbole
 Ns = Fe / Rs; % Facteur de sur échantillonnage
-nbits = 300; % Nombre de bits à transmettre
+nbits = 100 * log2(M); % Nombre de bits à transmettre
 
 rolloff = 0.35; % Roll-off du filtre de mise en forme
 span = 10; % Durée du filtre en symboles de base
 
+bits = randi([0, 1], 1, nbits); % Génération de l’information binaire
+
+%% Filtres
 h = rcosdesign(rolloff, span, Ns); % Génération de la réponse impulsionnelle du filtre de mise en forme
 hr = fliplr(h); % Génération de la réponse impulsionnelle du filtre de réception (filtrage adaptée)
 
-bits = randi([0, 1], 1, nbits); % Génération de l’information binaire
-
-%% Mapping
+%% Mapping PSK
 b = reshape(bits, log2(M), length(bits) / log2(M)); % Groupement des bits par paquets de log2(M) bits
-symboles = pskmod(b, M, pi / M, 'gray', 'InputType', 'bit', PlotConstellation = true)'; % Mapping des bits sur les symboles
+b = bit2int(b,log2(M)); % Conversion des bits groupés en entiers
+symboles = b';
+
+switch M % Mapping des bits sur les symboles
+    case 2
+        symboles(b == 0) = 1;
+        symboles(b == 1) = -1;
+    case 4
+        symboles(b == 0) = 1 + 1i; 
+        symboles(b == 1) = -1 + 1i;
+        symboles(b == 2) = 1 - 1i;
+        symboles(b == 3) = -1 - 1i;
+    case 8
+        symboles(b == 0) = exp(1i * pi / 8);
+        symboles(b == 1) = exp(3i * pi / 8);
+        symboles(b == 2) = exp(7i * pi / 8);
+        symboles(b == 3) = exp(5i * pi / 8);
+        symboles(b == 4) = exp(-1i * pi / 8);
+        symboles(b == 5) = exp(-3i * pi / 8);
+        symboles(b == 6) = exp(-7i * pi / 8);
+        symboles(b == 7) = exp(-5i * pi / 8);
+end
+
 
 
 diracs = kron(symboles', [1, zeros(1, Ns - 1)]); % Sur échantillonnage (génération de la suite de Diracs pondérés par les symboles)
 xe = filter(h, 1, diracs); % Filtrage de mise en forme (génération de l’enveloppe complexe associée au signal à transmettre)
-t = 0:Te:(length(xe)-1)*Te;
+t = 0:Te:(length(xe) - 1) * Te;
 x = real(xe .* exp(1i * 2 * pi * fp * t));
-plot(t, real(x));
 
+% Affichage des voies en phase et quadrature
+tiledlayout(2,1)
+nexttile
+plot(t, real(xe));
+xlabel("Temps (s)");
+ylabel("Amplitude");
+title("Voie en phase du signal")
+nexttile
+plot(t, imag(xe));
+xlabel("Temps (s)");
+ylabel("Amplitude");
+title("Voie en quadrature du signal")
+
+figure("Name", "Signal transmis");
+plot(t, x);
+xlabel("Temps (s)");
+ylabel("Amplitude");
+
+[DSP, f] = pwelch(x, [], [], [], Fe, 'centered');
+figure("Name", "DSP du signal transmis");
+semilogy(f, abs(DSP));
+xlabel("Fréquence (Hz)");
+ylabel("DSP (dB)");
+title("DSP du signal transmis");
+% Spectre avec deux bandes centrées en +fp et -fp, cohérent avec la théorie :
+% mettre sur porteuse décale le spectre initialisement modulé en bande de base
+% cf formule cours ( 1/4 * (S(-f-fp) + (S(f-fp)))
+
+figure("Name","Diagramme de l'oeil du signal en sortie")
+plot(reshape(x,Ns,length(x)/Ns))
+xlabel('Temps (s)')
+
+%% Canal awng
 Px = mean(abs(x) .^ 2); % Calcul de la puissance du signal transmis
 Pn = Px * Ns / (2 * log2(M) * 10 ^ (EbN0dB / 10)); % Calcul de la puissance du bruite à introduire pour travailler au niveau de Eb N0 souhaité
 n = sqrt(Pn) * randn(1, length(x)); % Génération du bruit
 r = x + n; % Ajout du bruit
 
-z = filter(hr, 1, r .* cos(2 * pi * fp * [0:Te:(length(r) - 1) * Te])); % Retour en bande de base avec filtrage passe-bas = filtre adapté
-n0 = Ns; % Choix de l’instant d’échantillonnage.
-zm = z(n0:Ns:end); % Echantillonnage à n0+mNs
-am = sign(real(zm)); % Décisions sur les symboles
-bm = (am + 1) / 2; % Demapping
+%% Réception
+% z = filter(hr, 1, r .* cos(2 * pi * fp * [0:Te:(length(r) - 1) * Te])); % Retour en bande de base avec filtrage passe-bas = filtre adapté
+% n0 = Ns; % Choix de l’instant d’échantillonnage.
+% zm = z(n0:Ns:end); % Echantillonnage à n0+mNs
+% am = sign(real(zm)); % Décisions sur les symboles
+% bm = (am + 1) / 2; % Demapping
 
-TEB = length(find((bm - bits) ~= 0)) / length(bits); % Calcul du TEB
+% BW = 2 * fp; % Taille du filtre passe-bas
+% N = 101; % Ordre du filtre passe-bas
+% WN = [(fp - BW/2) (fp + BW/2)]/(Fe/2);
+% h_pc = fir1(N, WN, 'bandpass');
+% z = filter(h_pc, 1, r); % Retour en bande de base avec filtrage passe-bas = filtre adapté
+z = r; % tkt on est seul au monde
+
+% Multiplication par cosinus / sinus
+z_cos = z .* cos(2*pi*fp*t);
+z_sin = z .* sin(2*pi*fp*t);
+
+% Filtrage passe-bas
+BW = 1.5 * fp;
+N = 101;
+h_pb = fir1(N, BW / (Fe / 2), "low"); % Filtre passe-bas
+
+z_cos = filter(h_pb, 1, z_cos);
+z_sin = filter(h_pb, 1, z_sin);
+
+% Combinaison des deux voies
+z_f = z_cos + 1i * z_sin;
+y = filter(hr, 1, z_f);
+
+% échantillonage
+N0 = Ns; 
+echantilloned = y(N0:Ns:length(y));
+plot(echantilloned, 'o');
+detected = zeros(length(echantilloned), 1);
+switch M
+    case 2
+        detected(real(echantilloned) > 0) = 1;
+        detected(real(echantilloned) > 0) = -1;
+    case 4
+        detected(real(echantilloned) > 0 & imag(echantilloned) > 0) = 3;
+        detected(real(echantilloned) > 0 & imag(echantilloned) <= 0) = 2;
+        detected(real(echantilloned) <= 0 & imag(echantilloned) > 0) = 1;
+        detected(real(echantilloned) <= 0 & imag(echantilloned) <= 0) = 0;
+    case 8
+        detected(angle(echantilloned) > 0 & angle(echantilloned) <= pi / 4) = 0;
+        detected(angle(echantilloned) > pi / 4 & angle(echantilloned) <= pi / 2) = 1;
+        detected(angle(echantilloned) > pi / 2 & angle(echantilloned) <= 3 * pi / 4) = 3;
+        detected(angle(echantilloned) > 3 * pi / 4 & angle(echantilloned) <= pi) = 2;
+        detected(angle(echantilloned) > -pi & angle(echantilloned) <= -3 * pi / 4) = 6;
+        detected(angle(echantilloned) > -3 * pi / 4 & angle(echantilloned) <= -pi / 2) = 7;
+        detected(angle(echantilloned) > -pi / 2 & angle(echantilloned) <= -pi / 4) = 5;
+        detected(angle(echantilloned) > -pi / 4 & angle(echantilloned) <= 0) = 4;        
+end
+
+% demapping
+demapped = int2bit(demapped, 2);
+demapped = reshape(demapped, 1, nbits);
+
+
+% TEB = length(find((bm - bits) ~= 0)) / length(bits); % Calcul du TEBz = filter(hr, 1, r .* cos(2 * pi * fp * [0:Te:(length(r) - 1) * Te])); % Retour en bande de base avec filtrage passe-bas = filtre adapté
+% n0 = Ns; % Choix de l’instant d’échantillonnage.
+% zm = z(n0:Ns:end); % Echantillonnage à n0+mNs
+% am = sign(real(zm)); % Décisions sur les symboles
+% bm = (am + 1) / 2; % Demapping
